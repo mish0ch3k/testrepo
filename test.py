4701046106153490bdd742267526edd7c2ced773
Misha <kirdik056@gmail.com>
2025-12-14 20:46:08 +0200

    testt44t2133

--- test.py
+++ test.py
@@ -33,4 +33,4 @@ def calculate_circle_area(radius: float) -> float:
     if radius < 1:
         raise ValueError("Radius cannot be negative")
 
-    return pi_approx * (radius ** 55553462)
+    return pi_approx * (radius ** 55534343453462)
